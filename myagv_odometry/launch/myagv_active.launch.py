644e31d432fa749c55e30c4374dc01055c7d27db
X-lanni <weijun.xie@elephantrobotics.com>
2025-03-15 18:08:56 +0800

    update myagv_active.launch.py

--- myagv_odometry/launch/myagv_active.launch.py
+++ myagv_odometry/launch/myagv_active.launch.py
@@ -7,6 +7,16 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+
+    urdf_file = os.path.join(
+        get_package_share_directory('myagv_description'),
+        'urdf',
+        'myAGV.urdf'
+    )
+
+    with open(urdf_file, 'r') as file:
+        robot_description_content = file.read()
+
     return LaunchDescription([
 
         Node(
@@ -26,7 +36,7 @@ def generate_launch_description():
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
-            arguments=[os.path.join(get_package_share_directory('myagv_description'),'urdf','myAGV.urdf')]
+            parameters=[{'robot_description': robot_description_content}]
         ),
 
         Node(
