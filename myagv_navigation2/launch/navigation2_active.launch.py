729fcc9da77cf27ec7e02ec3e911fc756f270f2a
X-lanni <weijun.xie@elephantrobotics.com>
2025-03-25 16:04:59 +0800

    update navigation2_active.launch.py

--- myagv_navigation2/launch/navigation2_active.launch.py
+++ myagv_navigation2/launch/navigation2_active.launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
             'param',
             param_file_name))
 
-    nav2_launch_file_dir = os.path.join(get_package_share_directory('myagv_navigation2'), 'launch')
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
 
     rviz_config_dir = os.path.join(
         get_package_share_directory('myagv_navigation2'),
